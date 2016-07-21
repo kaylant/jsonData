0891214abd9af4fc61ef3540ee4066c8dd768840
kaylant <kaylant@gmail.com>
2016-07-20 20:38:22 -0500

    initial commit

---
+++ jsonData/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  jsonData
+//
+//  Created by Kaylan Smith on 7/20/16.
+//  Copyright © 2016 Kaylan Smith. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
